77872d8f6e9ba9b185c265d2986d8e72e758f269
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-12-05 20:24:34 -0500

    Update readme

--- src/main.asm
+++ src/main.asm
@@ -4,17 +4,30 @@
 ; Author: Trevor Schiff (tschiff2020)
 ;         3 November 2023
 ;
-; Entry for the first CSE3120 assembly programming contest.
-;
 ; Some Win32 API functions are used, but only those which are included
 ; as part of the Irvine32 library.
 ;
 ; Help: Left-click to clear tiles, right-click to flag them (toggle).
+;
+;       For 5000 points, you can middle-click to use the "scanner" powerup.
+;       It reveals the targeted tile and its neighbors (green=OK, red=MINE).
+;   
 ;       The very first "clear" (left-click) will always be safe.
 ;       You win the game when all mines (and nothing else) are flagged.
 ;
 ;       To change the board size, zoom in/out in the Command Prompt window,
 ;       using CTRL+ScrollUp or CTRL+ScrollDown.
+;=============================================================================;
+; Updates:
+;
+; 3 November 2023 - First version.
+;                   Finished as entry for the first CSE3120 assembly
+;                   programming contest.
+;
+; 5 December 2023 - Finished as entry for the SECOND programming contest.
+;                   Added mine "scanner" power-up, costs 5000 points.
+;                   To purchase & use, middle-click a tile to reveal.
+;                   The tile and all its neighbors will be revealed.
 ;=============================================================================;
 
 INCLUDE Irvine32.inc
